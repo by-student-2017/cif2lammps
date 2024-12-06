4df6e3793f66908cce9c2b850e60463c79a8d8d5
by-student-2017 <33742249+by-student-2017@users.noreply.github.com>
2024-12-06 18:46:19 +0900

    Add files via upload

--- UFF4MOF_construction.py
+++ UFF4MOF_construction.py
@@ -249,17 +249,17 @@ class UFF4MOF(force_field):
                         elif len(nbors) == 3 and len([i for i in nbor_symbols if i in metals]) > 1:
                             # trigonal geometry
                             #if dist_triangle < dist_tetrahedral and not any(i in ['Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
-                            if dist_triangle < dist_tetrahedral and not any(i in ['Cd', 'Ni', 'Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
+                            if dist_triangle < dist_tetrahedral and not any(i in ['Mg', 'Cd', 'Ni', 'Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
                                 ty = 'O_2_z'
                                 hyb = 'sp2'
                             # sometimes oxygens in Zr6 and analagous nodes can have near trigonal geometry, still want O_3_f, however
                             #elif dist_triangle < dist_tetrahedral and any(i in ['Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
-                            elif dist_triangle < dist_tetrahedral and any(i in ['Cd', 'Ni', 'Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
+                            elif dist_triangle < dist_tetrahedral and any(i in ['Mg', 'Cd', 'Ni', 'Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
                                 ty = 'O_3_f'
                                 hyb = 'sp2'
                             # tetrahedral-like geometry
                             #elif dist_tetrahedral < dist_triangle and any(i in ['Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
-                            elif dist_tetrahedral < dist_triangle and any(i in ['Cd', 'Ni', 'Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
+                            elif dist_tetrahedral < dist_triangle and any(i in ['Mg', 'Cd', 'Ni', 'Zr', 'Eu', 'Tb', 'U'] for i in nbor_symbols):
                                 ty = 'O_3_f'
                                 hyb = 'sp3'
                         # 4 connected oxygens bonded to metals
